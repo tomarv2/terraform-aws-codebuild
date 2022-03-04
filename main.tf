4b2e0fb3a100ea4d09e5f9470059dfedb58138a2
tomarv2 <varuntomar2019@gmail.com>
2022-03-04 18:32:12 +0000

    Sync from management repo

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ module "cloudwatch_event" {
 }
 
 module "cloudwatch" {
-  source = "git::git@github.com:tomarv2/terraform-aws-cloudwatch.git?ref=v0.0.4"
+  source = "git::git@github.com:tomarv2/terraform-aws-cloudwatch.git?ref=v0.0.7"
 
   cloudwatch_path = var.cloudwatch_path
   custom_tags     = var.custom_tags
