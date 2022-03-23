74b820f641d24172c8fb7cdc43e457907b857b45
tomarv2 <varuntomar2019@gmail.com>
2022-03-23 14:18:54 +0000

    Sync from management repo

--- locals.tf
+++ locals.tf
@@ -1,10 +1,3 @@
 locals {
-  shared_tags = tomap(
-    {
-      "Name"    = "${var.teamid}-${var.prjid}",
-      "team"    = var.teamid,
-      "project" = var.prjid
-    }
-  )
   buildspec_filepath = var.buildspec_filepath != "" ? var.buildspec_filepath : "${path.module}/buildspec.yml"
 }
